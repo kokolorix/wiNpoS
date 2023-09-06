e17ced4ca8485bccf07fc81f949c2667c0a3d9c3
srdev <sr.dev@outlook.com>
2023-09-07 00:28:25 +0200

    initial commit load and unload the dll

---
+++ wiNpoS-Shared/Hooks.cpp
@@ -0,0 +1,23 @@
+#include "pch.h"
+#include "Hooks.h"
+#include <cassert>
+
+void Hooks::attach()
+{
+#ifdef Win32
+	_hModule = LoadLibrary(L"wiNpoS-Hook32.dll");
+#else
+	_hModule = LoadLibrary(L"wiNpoS-Hook64.dll");
+#endif // Win32
+
+}
+
+void Hooks::detach()
+{
+	assert(FreeLibrary(_hModule));
+//#ifdef Win32
+//	LoadLibrary(L"wiNpoS-Hook32.dll");
+//#else
+//	LoadLibrary(L"wiNpoS-Hook64.dll");
+//#endif // Win32
+}
